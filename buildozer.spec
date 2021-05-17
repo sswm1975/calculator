cbf6f3eece0148cabb74eb6fc4481373aa7797a0
SSWM <sswm1975@gmail.com>
2021-05-17 17:06:11 +0300

    Add buttons, add icons

--- buildozer.spec
+++ buildozer.spec
@@ -28,7 +28,7 @@ source.dir = .
 #source.exclude_patterns = license,images/*/*.jpg
 
 # (str) Application versioning (method 1)
-version = 0.2
+version = 0.3
 
 # (str) Application versioning (method 2)
 # version.regex = __version__ = ['"](.*)['"]
@@ -46,7 +46,7 @@ requirements = python3,kivy==2.0.0
 #presplash.filename = %(source.dir)s/data/logo/presplash512okmin.png
 
 # (str) Icon of the application
-#icon.filename = %(source.dir)s/data/logo/logo512min.png
+icon.filename = %(source.dir)s/logo512.png
 
 # (str) Supported orientation (one of landscape, sensorLandscape, portrait or all)
 orientation = all
--- main.py
+++ main.py
@@ -17,21 +17,53 @@ class Container(BoxLayout):
     label_input = ObjectProperty()
     formula = ''
 
-    def add_number(self, instance):
+    def add_in_formula(self, instance):
+        if self.label_info.text == 'Error':
+            self.label_info.color = 'yellow'
+            self.label_info.text = ''
+
         self.formula += str(instance.text)
         self.label_input.text = self.formula
 
-    def calc_result(self, instance):
-        self.label_info.text = self.formula
-        self.label_input.text = str(eval(self.formula))
+    def set_sign(self, instance):
+        if self.formula[0] == '-':
+            self.formula = self.formula[1:]
+        else:
+            self.formula = '-' + self.formula
+
+        self.label_input.text = self.formula
+
+    def clear_formula(self, instance):
         self.formula = ''
+        self.label_input.text = ''
+        self.label_info.text = ''
+
+    def calc_result(self, instance):
+        if self.formula == '':
+            return
+
+        try:
+            calc = str(eval(self.formula.replace('x', '*')))
+        except:
+            self.label_info.color = 'red'
+            self.label_info.text = 'Error'
+            calc = False
+
+        if calc:
+            if len(calc) > 2 and calc[-2:] == '.0':
+                calc = calc[:-2]
+
+            self.label_input.text = calc
+            self.label_info.text = self.formula + ' ='
+            self.formula = calc
 
 
 class CalculatorApp(App):
     title = "Калькулятор"
+    icon = "logo512.png"
+
     def build(self):
         return Container()
 
 
-if __name__ == '__main__':
-    CalculatorApp().run()
+CalculatorApp().run()
